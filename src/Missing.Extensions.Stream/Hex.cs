4ab6b09e8e5e14711308bedd17c9ff628279b19a
Oleksii Bielai <oleksii.bielai@gmail.com>
2025-05-10 07:14:44 +0300

    introduce Hex struct

---
+++ src/Missing.Extensions.Stream/Hex.cs
@@ -0,0 +1,20 @@
+namespace Missing.Extensions.Stream;
+
+internal readonly ref struct Hex
+{
+    private readonly ReadOnlySpan<char> _source;
+
+    private Hex(ReadOnlySpan<char> source)
+    {
+        if (int.DivRem(source.Length, 2) is not (> 0, 0))
+        {
+            throw new FormatException("The input is not a valid hex string.");
+        }
+
+        _source = source;
+    }
+
+    public static implicit operator Hex(string? source) => new(source?.Trim());
+
+    public int BytesCount => _source.Length >> 1;
+}
---
+++ test/Missing.Extensions.Stream.Tests/HexTests.cs
@@ -0,0 +1,23 @@
+namespace Missing.Extensions.Stream.Tests;
+
+public sealed class HexTests
+{
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("A")]
+    [InlineData("ABC")]
+    public void ShouldThrowFormatException(string? s) =>
+        Assert.Throws<FormatException>(() =>
+        {
+            Hex _ = s;
+        });
+
+    [Theory]
+    [InlineData("AB", 1)]
+    [InlineData("ABCD", 2)]
+    [InlineData("ABCDEF", 3)]
+    public void ShouldCountBytes(string s, int expected) =>
+        Assert.Equal(expected, ((Hex)s).BytesCount);
+}
