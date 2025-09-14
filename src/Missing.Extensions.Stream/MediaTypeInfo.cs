5ce66c53895490bc8445cfa7e34e06f0ffee9117
Oleksii Bielai <oleksii.bielai@gmail.com>
2025-09-14 20:47:48 +0300

    Refactoring

--- src/Missing.Extensions.Stream/MediaTypeInfo.cs
+++ src/Missing.Extensions.Stream/MediaTypeInfo.cs
@@ -1,11 +1,18 @@
 namespace Missing.Extensions.Stream;
 
 internal sealed class MediaTypeInfo(
-    string name,
-    string extension,
+    string nameInfo,
+    string extensionInfo,
     string[] signs)
 {
-    public string Name { get; } = name;
-    public string Extension { get; } = extension;
-    public string[] Signs { get; } = signs;
+    public void Deconstruct(
+        out string name, out string extension) =>
+        (name, extension) = (nameInfo, extensionInfo);
+
+    public bool IsMatch(System.IO.Stream stream) =>
+        Array.Exists(signs, sign => IsMatch(sign, stream));
+
+    private static bool IsMatch(
+        Sign sign, System.IO.Stream stream) =>
+        sign.IsMatch(stream);
 }
